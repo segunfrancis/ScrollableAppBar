3b3a34b133b751ab163fd2349ba0191ee5426596
segunfrancis <francis4dblues@gmail.com>
2019-04-26 23:58:32 +0100

    UI modifications

--- app/src/main/java/com/example/computer/scrollabletoolbar/ExitUntilCollapsedActivity.java
+++ app/src/main/java/com/example/computer/scrollabletoolbar/ExitUntilCollapsedActivity.java
@@ -2,12 +2,38 @@ package com.example.computer.scrollabletoolbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class ExitUntilCollapsedActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exit_until_collapsed);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        toolbar.setTitle(getLocalClassName());
+        toolbar.setNavigationIcon(R.drawable.ic_action_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        toolbar.inflateMenu(R.menu.main_menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.settings) {
+                    Toast.makeText(ExitUntilCollapsedActivity.this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/computer/scrollabletoolbar/MainActivity.java
+++ app/src/main/java/com/example/computer/scrollabletoolbar/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.computer.scrollabletoolbar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button scrollFlags, scrollEnterAlways, enterAlwaysCollapsed, exitUntilCollapsed;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scrollFlags = findViewById(R.id.scroll_flags);
+        scrollEnterAlways = findViewById(R.id.scroll_enter_always);
+        enterAlwaysCollapsed = findViewById(R.id.enter_always_collapsed);
+        exitUntilCollapsed = findViewById(R.id.exit_until_collapsed);
+        toolbar = findViewById(R.id.toolbar);
+
+        toolbar.setTitle(R.string.app_name);
+
+        scrollFlags.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScrollFlagActivity.class));
+            }
+        });
+
+        scrollEnterAlways.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScrollEnterAlwaysActivity.class));
+            }
+        });
+
+        enterAlwaysCollapsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EnterAlwaysCollapsedActivity.class));
+            }
+        });
+
+        exitUntilCollapsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExitUntilCollapsedActivity.class));
+            }
+        });
     }
 }
