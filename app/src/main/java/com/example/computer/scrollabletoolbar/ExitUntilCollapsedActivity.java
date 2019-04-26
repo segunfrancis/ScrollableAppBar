aa68f912c01d99246c6c2efe8c306eaecae3bea5
segunfrancis <francis4dblues@gmail.com>
2019-04-26 23:04:42 +0100

    Add CollapsedUntilExitActivity - modification

---
+++ app/src/main/java/com/example/computer/scrollabletoolbar/ExitUntilCollapsedActivity.java
@@ -0,0 +1,13 @@
+package com.example.computer.scrollabletoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExitUntilCollapsedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exit_until_collapsed);
+    }
+}
